7eda14be83fe9bbed9a3a8550cf67ed8319df750
jeansi07 <jeansi123@live.com>
2025-06-27 18:58:38 -0500

    bugfix: resolve error vercel

--- src/screen/home/pokemons/Pokemons.tsx
+++ src/screen/home/pokemons/Pokemons.tsx
@@ -10,7 +10,7 @@ import { PokemonCharts } from "../../../components/Charts";
 import Loader from "../../../components/Loader/Loader";
 import { Logo } from "../../../components/Logo";
 import PokemonsDetails from "../../../components/Modal/PokemonsDetails";
-import { Select } from "../../../components/Select";
+import { Select } from "../../../components/Select/Select";
 import { PokemonResponse } from "../../../interfaces/types";
 
 const ITEMS_PER_PAGE = 20;
