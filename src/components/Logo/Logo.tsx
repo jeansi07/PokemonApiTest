f9670c936e99d819f96432290e524683b2d12831
jeansi07 <jeansi123@live.com>
2025-06-28 13:18:56 -0500

    feat: change file

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ export const Logo = () => {
   return (
     <div>
       <img
-        src="src/assets/img/logo-pokemon.png"
+        src="public/img/logo-pokemon.png"
         alt="Pokedex Logo"
         className=" w-56 lg:w-96 mx-auto mb-4"
       />
